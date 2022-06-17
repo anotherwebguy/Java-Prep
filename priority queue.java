9265a59b3312869e971e927bfe820426ffe22b74
Mohit singh <mohitrajputms123@gmail.com>
2022-06-17 10:39:57 +0530

    Create priority queue.java

---
+++ priority queue.java
@@ -0,0 +1,6 @@
+static void kth_Largest_MaxHeap(int[] arr, int k) {
+
+        PriorityQueue<Integer> max= new PriorityQueue<>((a,b)->b-a);
+        PriorityQueue<Integer> min= new PriorityQueue<>();
+        
+    }
